806d661acc92adf49b6d2103a4658c2bc4a8bc6b
Benyamin Eslami <benyamin.es79@gmail.com>
2022-08-10 07:55:37 +0430

    try to make it cleaner

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 const getTableDatas = (id) => {
   let tableDataList = [];
-  let tableObj;
+  let tableObj = {};
   const trElements = document.querySelectorAll(
     `#${id + " " + "tbody" + " " + "tr"}`
   );
-  const thLists = Array.from(trElements[0].children);
 
-  const tableKeys = thLists.map((th) => th.innerHTML);
+  const tableKeys = Array.from(trElements[0].children).map(
+    (th) => th.innerHTML
+  );
 
   let rank = tableKeys[0];
   let city = tableKeys[1];
   let province = tableKeys[2];
   let population = tableKeys[3];
 
-  const subCityPopulationElement = Array.from(
-    trElements[1].children[0].textContent
-  )
-    .join("")
-    .trim()
-    .slice(0, 4);
-
-  const subMetroPopulationElement = Array.from(
-    trElements[1].children[1].textContent
-  )
-    .join("")
-    .trim()
-    .slice(0, 5);
+  let rankValue,
+    cityValue,
+    provinceValue,
+    populationValue,
+    populationCityValue,
+    populationMetroValue;
 
   for (let i = 2; i < trElements.length; i++) {
-    tableObj = {};
-
-    tableObj[rank] = Array.from(trElements[i].children[0].innerHTML)[0];
-
-    tableObj[city] = Array.from(
+    rankValue = Array.from(trElements[i].children[0].innerHTML)[0];
+    cityValue = Array.from(
       trElements[i].children[1].children[0].innerHTML
     ).join("");
-
-    tableObj[province] = Array.from(
+    provinceValue = Array.from(
       trElements[i].children[2].children[0].innerHTML
     ).join("");
+    populationCityValue = Array.from(trElements[i].children[3].innerHTML).join(
+      ""
+    );
+    populationMetroValue = Array.from(trElements[i].children[4].innerHTML).join(
+      ""
+    );
+    populationValue = {
+      city: populationCityValue,
+      Metro: populationMetroValue,
+    };
 
-    tableObj[population] = {
-      [subCityPopulationElement]: Array.from(
-        trElements[i].children[3].innerHTML
-      ).join(""),
-      [subMetroPopulationElement]: Array.from(
-        trElements[i].children[4].innerHTML
-      ).join(""),
+    tableObj = {
+      [rank]: rankValue,
+      [city]: cityValue,
+      [province]: provinceValue,
+      [population]: populationValue,
     };
 
     tableDataList.push(tableObj);
